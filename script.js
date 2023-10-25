d76b84bf69434d3948bc798bc0c5a6630a0884c0
giantflyingegg <abwaham@gmail.com>
2023-10-25 14:27:38 +0100

    Fixed styling and navbar burger, needs active links

--- script.js
+++ script.js
@@ -1,82 +1,91 @@
-window.onload = function() {
-    // Target canvas element and ctx
-    const canvas = document.getElementById('bannerCanvas');
-    const ctx = canvas.getContext('2d');
+// Target canvas element and ctx
+const canvas = document.getElementById('bannerCanvas');
+const ctx = canvas.getContext('2d');
+
+// Create image object
+const img = new Image();
+img.src = 'Images/Banner.png'; 
+
+// Function to draw on the canvas
+function draw() {
+    // Calculate the aspect ratio
+    const aspectRatio = img.width / img.height;
+
+    // Set the canvas height based on the calculated aspect ratio
+    canvas.height = canvas.width / aspectRatio;
+
+    // Draw the image
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+    // Set heading font, size, and style
+    ctx.font = 'bold 150px Arial'; 
     
-    //Set width and height
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    // Text colour
+    ctx.fillStyle = 'black';
 
-    // Create image object
-    const img = new Image();
-    img.src = 'Images/Banner.png'; 
+    // Text coordinates
+    const headX = 70;  
+    const headY = 500;  
 
-    // Await image load and then draw
-    img.onload = function() {
-
-        // Calculate the aspect ratio 
-        const aspectRatio = img.width / img.height;
-
-        // Set the canvas height based on the calculated aspect ratio
-        canvas.height = canvas.width / aspectRatio;
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-            
-        // Set heading font, size, and style
-        ctx.font = 'bold 150px Arial'; 
-    
-        // Text colour
-        ctx.fillStyle = 'black';
-
-        // Text coordinates
-        const headX = 70;  
-        const headY = 500;  
-
-        // Display heading text
-        ctx.fillText('About me', headX, headY);
-        ctx.strokeStyle = 'beige';  // Outline color
-        ctx.lineWidth = 2;  // Outline thickness
-        ctx.strokeText('About me', headX, headY); ///Draw ouutline
+    // Display heading text
+    ctx.fillText('About me', headX, headY);
+    ctx.strokeStyle = 'beige';  // Outline color
+    ctx.lineWidth = 2;  // Outline thickness
+    ctx.strokeText('About me', headX, headY); ///Draw ouutline
         
-        // Set font, size, and style
-        ctx.font = 'bold 60px Arial'; 
+    // Set font, size, and style
+    ctx.font = 'bold 60px Arial'; 
     
-        // Text colour
-        ctx.fillStyle = 'black';
+    // Text colour
+    ctx.fillStyle = 'black';
         
-        // Text coordinates
-        let textX = 70;  
-        let textY = 600;
+    // Text coordinates
+    let textX = 70;  
+    let textY = 600;
 
-            // Gap between lines
-        const lineGap = 80; 
+    // Gap between lines
+    const lineGap = 80; 
 
-        // Array containing the lines of text
-        const lines = [
-        "Dr Kieran Sweetman",
+    // Array containing the lines of text
+    const lines = [
+        "Kieran Sweetman",
         "Noob Software Developer",
         "Web3 Enthusiast",
         "DeSci and Global Health Advocate"
         ];
 
-        // Loop for lines of text
-        for (let i = 0; i < lines.length; i++) {
-        ctx.fillText(lines[i], textX, textY);
-        ctx.strokeText(lines[i], textX, textY); ///Draw outline
-        ctx.strokeStyle = 'beige';  // Outline color
-        ctx.lineWidth = 2;  // Outline thickness
-        textY += lineGap;  
-    }
+    // Loop for lines of text
+    for (let i = 0; i < lines.length; i++) {
+            ctx.fillText(lines[i], textX, textY);
+            ctx.strokeText(lines[i], textX, textY); ///Draw outline
+            ctx.strokeStyle = 'beige';  // Outline color
+            ctx.lineWidth = 2;  // Outline thickness
+            textY += lineGap;  
+        }
 }; 
 
-}
-        
+// Event listener for image load
+img.onload = draw;
 
-//Adapt to window resize
+// Set up the canvas when the window has loaded
+window.onload = function() {
+    // Set canvas width
+    canvas.width = window.innerWidth;
+
+    draw();
+};
+
+// Event listener for window resize
 window.addEventListener('resize', function() {
     // Update the canvas width
     canvas.width = window.innerWidth;
+    // console.log('resize');
 
-    // Redraw the image
-    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    draw();
 });
 
+//toggle burger menu when clicked
+function toggleMenu() {
+    const navLinks = document.querySelector('.navbar ul');
+    navLinks.classList.toggle('show');
+}
