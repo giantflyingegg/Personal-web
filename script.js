3e736c5ea3c500563c6f7183eb666b17dfe3ba37
giantflyingegg <abwaham@gmail.com>
2023-11-02 13:43:30 +0000

    fade in effect working

--- script.js
+++ script.js
@@ -129,3 +129,19 @@ function toggleMenu() {
     const navLinks = document.querySelector('.navbar ul');
     navLinks.classList.toggle('show');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    fadeInElements.forEach(element => {
+        observer.observe(element);
+    });
+});
